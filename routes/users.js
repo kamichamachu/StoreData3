fb9b80fea3b4361c198f2097fc1d1047b51bc4ff
kamichamachu <gharvie@horizon.csueastbay.edu>
2018-05-15 08:34:05 -0700

    make it better

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-//now processing post
-router.post('/storeData', function(req, res, next) {
-//expecting data variable called order--retrieve value using body-parser
-    var value_name = req.body.order  //retrieve the data associated with order
-    res.send("order succesfully received: " + value_name);
-});
